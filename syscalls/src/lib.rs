53b4acfec9271ac5111d6e753caca05108992f40
thelonehegelian <thelonehegelian@gmail.com>
2023-06-26 22:54:41 +0100

    Fix missing crate errors

--- syscalls/src/lib.rs
+++ syscalls/src/lib.rs
@@ -1,10 +1,17 @@
+extern crate libc;
+use libc::c_int;
 use nix::sys::wait::waitpid;
 use nix::unistd::{execvp, fork, ForkResult};
+use std::fs::File;
+use std::io::{self, Read};
+use std::os::unix::io::FromRawFd;
 
 // @note where does the user provide the prompt for the system call?
+// https://github.com/Stichting-MINIX-Research-Foundation/minix/blob/master/minix/servers/pm/exec.c
 mod system_calls {
+    use super::*;
     // SYS_FORK
-    pub fn sys_fork() -> ForkResult {
+    pub fn sys_fork() {
         let pid = unsafe { fork() };
         match pid {
             Ok(ForkResult::Parent { child, .. }) => {
@@ -21,10 +28,39 @@ mod system_calls {
                 std::process::exit(1);
             }
         }
+        // unimplemented!();
     }
 
-    pub fn sys_exec() -> Result {
+    // SYS_EXEC takes a command and executes it
+    // @todo
+    pub fn sys_exec() -> Result<(), nix::Error> {
+        unimplemented!();
 
         // execvp(command_args[0], &command_args).expect("failed to execute command");
+    }
+
+    // SYS_EXIT
+    pub fn sys_exit() {
+        println!("Exiting");
+        std::process::exit(1);
+    }
+
+    pub fn sys_wait() {
+        //  Suspend the current process until one of its child processes terminates
+    }
+
+    // SYS_OPEN takes a file path and opens it for reading or writing or both
+    pub fn sys_open(path: &str) -> io::Result<String> {
+        let mut file = File::open(path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        Ok(contents)
+    }
+
+    // SYS_CLOSE closes a file descriptor
+    // @note Rust automatically closes files when they go out of scope
+    // but we can do it explicitly with this system call
+    fn sys_close(fd: c_int) -> c_int {
+        unsafe { libc::close(fd) }
     }
 }
